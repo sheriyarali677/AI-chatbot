e8579027002f4bd7dcd610957be7b0f24f89213c
Sheriyar <shero556@live.com>
2022-08-07 03:09:31 +0100

    final

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 # Importing Necessary Modules
 import json
 import random
+from statistics import mode
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
@@ -11,7 +12,9 @@ from neuralnet import Modelnet
 from nltk_ls import bag_of_words
 from nltk_ls import tokenize
 from nltk_ls import stem
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from sklearn.svm import SVC
 with open('dataintents.json', 'r') as f:#load in dataset
     dataints = json.load(f)
 
@@ -55,6 +58,21 @@ for (pat_sent, tag) in xy:
 
 y_train_data = np.array(y_train_data)
 X_train_data = np.array(X_train_data)
+#X_train_data, X_test, y_train_data, y_test = train_test_split(X_train_data, y_train_data, test_size=0.25, random_state=42)
+
+#y_test = y_train_data[:1000]
+#x_test = X_train_data[:1000]
+
+#svc = SVC(kernal='linear')
+#svc.fit(X_train_data, y_train_data)
+# 
+# epochs:
+#   preds = model(y_test)
+#   precision = precision(x_test, preds)
+#   acc
+#   ...
+#   Print(..)
+
 
 # parameters 
 size_batch = 8
@@ -92,6 +110,7 @@ crit = nn.CrossEntropyLoss()
 
 # Train the model
 for epoch in range(epoch_count):
+    correct = 0
     for (words, lbls) in train_loader:
         
         lbls = lbls.to(dtype=torch.long).to(device)
@@ -99,17 +118,22 @@ for epoch in range(epoch_count):
         
         # Forward pass
         outputs = model(words)
-        # lbls = torch.max(lbls, 1)[1]
+       # lbls = torch.max(lbls, 1)
         loss = crit(outputs, lbls)
         
         # Backward and optimize
         opt.zero_grad()
         loss.backward()
-        opt.step()
-        
-    if (epoch+1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{epoch_count}], Loss: {loss.item():.4f}')
+        opt.step()   
 
+        classes = torch.argmax(outputs, dim=1)
+        correct += (classes == lbls).float().sum()
+    
+    accuracy = 100 * correct / len(X_train_data)
+
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{epoch_count}], Loss: {loss.item():.4f}, Accuracy: {accuracy}')
+      #  print('Precision: %.3f' % precision_score(y_test, y_pred))
 
 print(f'final loss: {loss.item():.4f}')
 
